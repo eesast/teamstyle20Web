80f28bdd211c4f0482b9bccdbe8c8e95c9dc040e
Your Name <315629555@qq.com>
2019-03-12 22:07:01 +0800

    Add battle.

--- backend/team20Web/urls.py
+++ backend/team20Web/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from backend import views
 
 urlpatterns = [
+	path('battle/', include('battle.urls')),
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('announcement/<int:post_id>', views.detail, name='announcement_detail'),
