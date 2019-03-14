4f7422b4c351d8b19ba810231705e2789006cc15
BaronYou <baron0426@gmail.com>
2019-03-14 12:47:30 +0800

    add static
    add static

--- backend/team20Web/settings.py
+++ backend/team20Web/settings.py
@@ -76,13 +76,13 @@ WSGI_APPLICATION = 'team20Web.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'teamstyle20',
-        'USER': 'root',
-        'PASSWORD': '321A@eesast$THU',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.mysql',
+        #'NAME': 'teamstyle20',
+        #'USER': 'root',
+        #'PASSWORD': '321A@eesast$THU',
+        #'HOST': 'localhost',
     }
 }
 
