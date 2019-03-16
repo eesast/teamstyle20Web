a308b3517ab055fc128070f824e2956ad853303c
silentsaber <805898882@qq.com>
2019-03-16 13:24:50 +0800

    x

--- frontend/web2018/src/App.vue
+++ frontend/web2018/src/App.vue
@@ -7,6 +7,26 @@
 </template>
 
 <script>
+function getCookie(cname){
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name)==0) { return c.substring(name.length,c.length); }
+    }
+    return "";
+}
+
+function delCookie(name)
+{
+var exp = new Date();
+exp.setTime(exp.getTime() - 1);
+var cval=getCookie(name);
+if(cval!=null)
+document.cookie= name + "="+cval+";expires="+exp.toGMTString()+";path=/";
+}
+
+
 import navbar from './components/nav'
 import foot from './components/foot'
 
