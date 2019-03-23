45cb8925f1d4f8e34f4b68996cb096d4547bef98
BaronYou <baron0426@gmail.com>
2019-03-23 15:57:52 +0800

    .gitignore fixed

--- backend/battle/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Battle2Config(AppConfig):
-    name = 'battle2'
--- backend/battle/request.py
+++
@@ -1,7 +0,0 @@
-import requests ;
-
-payload = {'key': ''}
-url = 'http://127.0.0.1:8000/api/battle/end'
-r = requests.post(url, data=dumps(payload))
-print(r.status_code)
-print(r.text)
