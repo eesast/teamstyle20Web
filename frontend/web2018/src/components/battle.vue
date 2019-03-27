0ceeebef9f4707a59108547b79a475d58a6342f8
silentsaber <805898882@qq.com>
2019-03-28 00:21:18 +0800

    test

--- frontend/web2018/src/components/battle.vue
+++ frontend/web2018/src/components/battle.vue
@@ -297,52 +297,53 @@ export default {
           var form = new FormData();
           form.append(this.value.toString(), content.file);
 
-          var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://teamstyle.eesast.com/api/codes/teams/"+this.teamid,
-            "method": "POST",
-            "headers": {
-              "x-access-token": JSON.stringify({"token":token,"id":id,"username":username,"auth":true}),
-              "cache-control": "no-cache",
+          // var settings = {
+          //   "async": true,
+          //   "crossDomain": true,
+          //   "url": "https://teamstyle.eesast.com/api/codes/teams/"+this.teamid,
+          //   "method": "POST",
+          //   "headers": {
+          //     "x-access-token": JSON.stringify({"token":token,"id":id,"username":username,"auth":true}),
+          //     "cache-control": "no-cache",
           
-            },
-            "processData": false,
-            "contentType": false,
-            "mimeType": "multipart/form-data",
-            "data": form
-          }
-
-          $.ajax(settings).done(function (response) {
-            console.log(response);
-          });
-
-          // fetch("/api/codes/teams/"+this.teamid,{
-          //   method:'POST',
-          //   headers:{
-          //     "Content-Type": "multipart/form-data",
-          //      "x-access-token":JSON.stringify({"token":token,"id":id,"username":username,"auth":true})
           //   },
-          //   body:form,
-          // }).then(response=>{
-          //   this.fileList=[];
-          //   if(response.status=="204")
-          //   {
-          //     this.$message.success('上传成功!');             
-          //   }
-          //   else if(response.status=="401")
-          //   {
-          //     this.$message.error('你不在这个组中!');
-          //   }
-          //   else if(response.status=="403")
-          //   {
-          //     this.$message.error('不在系统开放时间内!');
-          //   }
-          //   else 
-          //   {
-          //     this.$message.error('上传失败!');
-          //   }
-          // })
+          //   "processData": false,
+          //   "contentType": false,
+          //   "mimeType": "multipart/form-data",
+          //   "data": form
+          // }
+
+          // $.ajax(settings).done(function (response) {
+          //   console.log(response);
+          // });
+
+          fetch("/api/codes/teams/"+this.teamid,{
+            method:'POST',
+            headers:{
+              // "content-type": "multipart/form-data",
+               "Content-Type": "application/json",
+              "x-access-token":JSON.stringify({"token":token,"id":id,"username":username,"auth":true})
+            },
+            body:form,
+          }).then(response=>{
+            this.fileList=[];
+            if(response.status=="204")
+            {
+              this.$message.success('上传成功!');             
+            }
+            else if(response.status=="401")
+            {
+              this.$message.error('你不在这个组中!');
+            }
+            else if(response.status=="403")
+            {
+              this.$message.error('不在系统开放时间内!');
+            }
+            else 
+            {
+              this.$message.error('上传失败!');
+            }
+          })
 
           }).catch(() => {
             this.fileList=[];
