354858f620528978fd881f0bb6abf077d708015a
baron0426 <baron0426@gmail.com>
2019-03-29 00:13:57 +0800

    server

--- backend/battle/urls.py
+++ backend/battle/urls.py
@@ -5,12 +5,12 @@ urlpatterns = [
     path('add', views.add_battle),
     path('end', views.end_battle),
     path('compile', views.compile),  #暂时写在battle里面
-    path('debug/view/queue', views.debug_view_queue),
+#    path('debug/view/queue', views.debug_view_queue),
     path('debug/view/team', views.debug_view_team),
-    path('debug/view/battle', views.debug_view_battle),
-    path('debug/clear/queue', views.debug_clear_queue),
-    path('debug/clear/team', views.debug_clear_team),
-    path('debug/clear/battle', views.debug_clear_battle),
-    path('debug/add/team', views.debug_add_team),
+#    path('debug/view/battle', views.debug_view_battle),
+#    path('debug/clear/queue', views.debug_clear_queue),
+#    path('debug/clear/team', views.debug_clear_team),
+#    path('debug/clear/battle', views.debug_clear_battle),
+#    path('debug/add/team', views.debug_add_team),
     path('result', views.view_result),
 ]
--- backend/media/Codes/44_0.cpp
+++ backend/media/Codes/44_0.cpp
@@ -1,8 +1,118 @@
-#include<cstdio>
-using namespace std;
+#include "api.h"
+#include "base.h"
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
 
-int main()
+using namespace ts20;
+
+extern XYPosition start_pos, over_pos;
+extern std::vector<int> teammates;
+extern int frame;
+extern PlayerInfo info;
+
+void play_game()
 {
 	
-	return 0;
- } 
+	update_info();
+	std::cout << "player:frame" << frame << "\nhp:" << info.self.hp << std::endl;
+	std::cout << "positon" << info.self.xy_pos.x << ' ' << info.self.xy_pos.y << std::endl;
+	if (!info.self.bag.empty())
+		std::cout << "pick succeed" << std::endl;
+	if (frame == 0)
+	{
+		srand(time(nullptr) +teammates[0]);
+		XYPosition landing_point = { rand() % 100 + 450, rand() % 100 + 450 };
+		parachute(HACK, landing_point);
+		return;
+	}
+	else
+	{
+		srand(time(nullptr) + info.player_ID*frame);
+	}
+	if (info.self.status == ON_PLANE || info.self.status == JUMPING)
+	{
+		std::cout << "jumping" << std::endl;
+		return;
+	}
+	if (info.others.empty())
+	{
+		std::cout << "no others" << std::endl;
+		if (info.items.empty())
+		{
+			//see nothing
+			double move_angle = 0;
+			double view_angle = move_angle;
+			move(move_angle, view_angle);
+			std::cout << "move" << move_angle << std::endl;
+		}
+		else
+		{
+			Item closest_item;
+			closest_item.polar_pos.distance = 100000;
+			for (int i = 0; i < info.items.size(); ++i)
+			{
+				if (info.items[i].polar_pos.distance < closest_item.polar_pos.distance)
+				{
+					closest_item = info.items[i];
+				}
+			}
+			std::cout << "status" << info.self.status << std::endl;
+			if (closest_item.polar_pos.distance < 1)
+			{
+				pickup(closest_item.item_ID);
+				std::cout << "try pickup" << closest_item.item_ID << std::endl;
+			}
+			else
+			{
+				move(closest_item.polar_pos.angle, closest_item.polar_pos.angle);
+				std::cout << "move" << closest_item.polar_pos.angle << std::endl;
+			}
+		}
+	}
+	else
+	{
+		bool has_enemy = false;
+		OtherInfo closest_enemy;
+		closest_enemy.polar_pos.distance = 100000;
+		//check teammate
+		for (int i = 0; i < info.others.size(); ++i)
+		{
+			bool is_friend = false;
+			for (int teammate = 0; teammate < teammates.size(); ++teammate)
+			{
+				if (info.others[i].player_ID == teammates[teammate])
+				{
+					is_friend = true;
+					break;
+				}
+			}
+			if (!is_friend && info.others[i].polar_pos.distance < closest_enemy.polar_pos.distance)
+			{
+				closest_enemy = info.others[i];
+				has_enemy = true;
+			}
+		}
+		if (has_enemy)
+		{
+			ITEM weapon = FIST;
+			for (int i = 0; i < info.self.bag.size(); ++i)
+			{
+				if (ITEM_DATA[info.self.bag[i].type].type == WEAPON && info.self.bag[i].durability > 0)
+				{
+					weapon = info.self.bag[i].type;
+					break;
+				}
+			}
+			if (closest_enemy.polar_pos.distance > ITEM_DATA[weapon].range)
+			{
+				move(closest_enemy.polar_pos.angle, closest_enemy.polar_pos.angle);
+			}
+			else
+			{
+				shoot(weapon, closest_enemy.polar_pos.angle);
+			}
+		}
+	}
+	return;
+}
