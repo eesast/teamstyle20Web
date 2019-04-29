5ac38045b9154ec316628e53e7e6a18fd434b7e4
BaronYou <baron0426@gmail.com>
2019-04-29 22:58:01 +0800

    fixing
    fixing

--- frontend/web2018/src/components/battle.vue
+++ frontend/web2018/src/components/battle.vue
@@ -53,7 +53,7 @@
             </el-upload>
             <!-- <el-input type="file" @onchange="jsReadFiles(this.files)"/> -->
             <h5><i class="el-icon-info"></i>系统仅保留最后一次上传的结果</h5>
-            <h5><span style="color:red"><i class="el-icon-info"></i>代码与对战截止日期:</span></h5>
+            <h5><span style="color:red"><i class="el-icon-info"></i>代码与对战截止日期:{{finaldate}}</span></h5>
             <br/>
             <h4>发起对战</h4>
             <h5><i class="el-icon-info"></i>仅可以选择代码<span style="color:red">有效</span>的队伍进行对战</h5>
@@ -142,14 +142,6 @@
 </template>
 
 <script>
-function datetodate(onedate)
-{
-    var T=onedate.indexOf('T');
-    var dot=onedate.indexOf('.');
-    var newdate=onedate.substring(0,T)+' '+onedate.substring(T+1,dot);
-    return newdate;  
-}
-
 function getCookie(cname) {
   var name = cname + "=";
   var ca = document.cookie.split(";");
@@ -188,7 +180,6 @@ export default {
             battle_time:10,//
             tableData: [],
             finaldate:null,
-			finaldateShow:null,
             //[{
             // teamname:'划水萌新',
             // captain:'萌新1号',
@@ -231,7 +222,6 @@ export default {
     created: function()
     {
       this.finaldate=new Date("2019-04-25T12:00:00+08:00");
-	  this.finaldateShow = datetodate(this.finaldate);
       fetch('/api/global',{
         method:'GET',
         headers:{
@@ -246,7 +236,6 @@ export default {
          else throw 'bad'
       }).then(res=>{
           this.finaldate=new Date(res.submission_end);
-		  this.finaldateShow = datetodate(this.finaldate);
       }).catch(()=>{this.$message.error('服务器无响应!')})
 
         fetch('/api/teams/0/members/'+id,
